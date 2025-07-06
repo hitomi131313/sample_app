118ad60791099e5fcf35f7c9499e8345040a745e
hitomi131313 <214909079+hitomi131313@users.noreply.github.com>
2025-07-06 13:25:05 +0000

    [Finish]3-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,20 @@
 class ListsController < ApplicationController
   def new
+    # View(new.html.erb)へ渡すためのインスタンス変数
+    # インスタンス変数に空のModelオブジェクト(とは？)を生成する
+    @list = List.new
+  end
+
+
+  # 最初にまとめてcreateアクションを作成しなかったのは、viewファイルに保存機能のcreateファイルはいらないから？
+  # 対応するviewファイルがなくて受け渡しも必要ないから＠でインスタンス変数にする必要がなく、ローカル変数を利用
+  def create
+    # 入力されたデータを受け取り、そのデータをもとに、新規登録するためのインスタンスを作成
+    # ↑のデータをデータベースに保存するために、↑で作成されたインスタンスにsaveメソッドを実行
+    # top画面へ遷移(リダイレクト)
+    list = List.new(list_params)
+    list.save
+    redirect_to'/top'
   end
 
   def index
@@ -9,5 +24,12 @@ class ListsController < ApplicationController
   end
 
   def edit
+  end
+
+  # privateは一種の境界線(⁼ここから下はこのcontrollerの中でしか呼び出せません)
+  private
+  # ストロングパラメータ(保存するデータの絞り込み)
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
