b76049ca2070ea6680b91cb51621ee6e8cb6b275
hitomi131313 <214909079+hitomi131313@users.noreply.github.com>
2025-07-08 13:16:06 +0000

    [finish]3-6

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -47,6 +47,6 @@ class ListsController < ApplicationController
   private
   # ストロングパラメータ(保存するデータの絞り込み)
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
